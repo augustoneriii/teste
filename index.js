826b57d5f12d7090f86faec2c006cfd6abfcc7a5
Augusto Neri <augusto.neri@senaimt.ind.br>
2023-09-13 16:18:18 -0400

    ahsu

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 
-const app = express()
+const app = express();
 
 // Config JSON response
-app.use(express.json())
+app.use(express.json());
 
 // Solve CORS
-app.use(cors({ credentials: true, origin: '*' }))
+app.use(cors({ credentials: true, origin: '*' }));
 
 // Public folder for images
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'teste ok' })
-})
+  res.status(200).json({ message: 'teste ok' });
+});
 
-app.listen(5000)
+app.listen(5000);
